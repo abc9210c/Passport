dcf418a0362af5df4c6b8e5adb06fb74d5053a96
George Hsieh <abc9210c@gmail.com>
2020-06-05 01:58:48 +0800

    fix

--- passport.py
+++ passport.py
@@ -1,12 +1,12 @@
 password = "bc0227"
 p = 3
 while p > 0:
+	p = p - 1
 	password = input('請輸入密碼: ')
 	if password == "bc0227":
 		print('登入成功!')
 		break
 	else:
-		p = p - 1
 		print('密碼錯誤!') 
 		if p > 0:
 			print('還剩', p, '次機會')
